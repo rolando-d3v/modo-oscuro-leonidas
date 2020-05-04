c16eb9b84a4d3801302d698ebfa324fc4dbaaf11
raidentek <33987212+raidentek@users.noreply.github.com>
2020-05-03 22:03:46 -0500

    agrege titulo

--- src/header.js
+++ src/header.js
@@ -8,7 +8,7 @@ function Header({ children }) {
       <div className="wrapper">
         <div className="header-grid">
           <div>
-            <h1>Social Media Dashboardx</h1>
+            <h1>Social Media Dashboard de Rolando</h1>
             <p className="header-total">Total Followers: 23, 004</p>
           </div>
           {children}
